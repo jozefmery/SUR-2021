80ddad2e906bd0d38595dc06cf8c45fe00bf8432
Dodmeister <jozefdmery@gmail.com>
2021-04-14 23:02:34 +0200

    fixed tensorflow warning, added more testing code

--- src/facerecognition.py
+++ src/facerecognition.py
@@ -1,13 +1,26 @@
 
 from keras.models import load_model
+import os
 
 def main():
 
+  os.environ["TF_XLA_FLAGS"] = "--tf_xla_enable_xla_devices"
+
   # load the model
-  model = load_model("../model/face/pretrained/facenet_keras.h5")
+
+  # do not compile, as pre-trained model has no training config
+  model = load_model("../model/face/pretrained/facenet_keras.h5", compile=False)
+ 
+  # compile manually
+  # https://www.tensorflow.org/api_docs/python/tf/keras/Model#compile
+  model.compile()
+
+  ####################
+  # TESTING CODE
   # summarize input and output shape
   print(model.inputs)
   print(model.outputs)
+  ####################
 
 if __name__ == "__main__":
   main()
