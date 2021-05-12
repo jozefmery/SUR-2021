b82bc49988c122e7ebca25a6a51fd6a2a17d3eee
Dodmeister <jozefdmery@gmail.com>
2021-05-12 22:42:58 +0200

    added model loading/saving and result saving

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import logging as log
+import pickle
+from pathlib import Path
 
 # call before importing tf or keras
 def set_tf_loglevel_warn():
@@ -20,3 +22,24 @@ def set_tf_loglevel(level):
 def enable_xla_devices():
   # https://www.tensorflow.org/xla
   os.environ["TF_XLA_FLAGS"] = "--tf_xla_enable_xla_devices"
+
+def mkdir(path):
+  Path(path).mkdir(parents=True, exist_ok=True)
+
+def load_model(path):
+  return pickle.load(open(path, "rb"))
+
+def save_model(path, model):
+  # create target directory if needed
+  mkdir(os.path.dirname(path))
+  pickle.dump(model, open(path, "wb"))
+
+def NaNs_string(n):
+  return " ".join(["NaN"] * n)
+
+def write_results(path, ids, predictions):
+  mkdir(os.path.dirname(path))
+  results = "\n".join(["{} {} {}".format(ident, pred, NaNs_string(31)) for ident, pred in zip(ids, predictions)])
+
+  with open(path, "w") as f:
+    f.write(results)
